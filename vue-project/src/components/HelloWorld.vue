642e9c69a40a00284a2ab1d665beca9763029dd1
Matheus Alves <matheusalvessilvabr@gmail.com>
2023-08-12 17:15:57 -0300

    switch to vue composition

--- vue-project/src/components/HelloWorld.vue
+++ vue-project/src/components/HelloWorld.vue
@@ -1,64 +1,63 @@
 <template>
-  <div id="app">
-    <h1>Lista de Tarefas</h1>
-    <div>
-      <input v-model="newTask" placeholder="Digite uma nova tarefa">
-      <button @click="addTask">Criar Tarefa</button>
-      <ul>
-        <li v-for="task in tasks" :key="task.id">
-          {{ task.title }}
-          <button @click="deleteTask(task.id)">Deletar</button>
-        </li>
-      </ul>
-    </div>
+  <h1>Lista de Tarefas</h1>
+  <div>
+    <input v-model="newTask" @keyup.enter="addTask" placeholder="Digite uma nova tarefa"/>
+    <button @click="addTask">Add Task</button>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.title }}
+        <button @click="deleteTask(task.id)">Delete</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      newTask: "",
-      tasks: []
-    };
-  },
-  methods: {
-    fetchTasks() {
-      fetch("https://jsonplaceholder.typicode.com/todos")
-        .then(response => response.json())
-        .then(data => {
-          this.tasks = data;
-        });
-    },
-    addTask() {
-      if (this.newTask.trim() === "") return;
-      
-      const newTask = {
-        userId: 1,
-        title: this.newTask,
-        completed: false
+  import { ref } from 'vue';
+
+  export default {
+    name: 'TaskList',
+    setup() {
+      const newTask = ref('');
+      const tasks = ref([]);
+
+      const addTask = () => {
+        if (newTask.value.trim() === '') return;
+
+        const taskToAdd = {
+          userId: 1,
+          title: newTask.value,
+          completed: false
+        };
+
+        fetch('https://jsonplaceholder.typicode.com/todos', {
+          method: 'POST',
+          body: JSON.stringify(taskToAdd),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+          .then(response => response.json())
+          .then(data => {
+            tasks.value.push(data);
+            newTask.value = '';
+          });
       };
-      
-      fetch("https://jsonplaceholder.typicode.com/todos", {
-        method: "POST",
-        body: JSON.stringify(newTask),
-        headers: {
-          "Content-Type": "application/json"
-        }
-      }).then(response => response.json())
-        .then(data => {
-          this.tasks.push(data);
-          this.newTask = "";
-          console.log(data);
+
+      const deleteTask = taskId => {
+        (`https://jsonplaceholder.typicode.com/todos/${taskId}`, {
+          method: 'DELETE'
+        }).then(() => {
+          tasks.value = tasks.value.filter(task => task.id !== taskId);
         });
-    },
-    deleteTask(taskId) {
-      fetch(`https://jsonplaceholder.typicode.com/todos/${taskId}`, {
-        method: "DELETE"
-      }).then(() => {
-        this.tasks = this.tasks.filter(task => task.id !== taskId);
-      });
+      };
+
+      return {
+        newTask,
+        tasks,
+        addTask,
+        deleteTask
+      };
     }
-  },
-};
+  };
 </script>
