4c6dac0110c01ccfa16e30ca65e42eb0ad18fea9
matheus alves <matheusalvessilvabr@gmail.com>
2023-08-14 16:48:41 -0300

    readme

--- vue-project/src/components/Home.vue
+++ vue-project/src/components/Home.vue
@@ -1,3 +1,13 @@
 <template>
     <h1>home</h1>
+
+    <button>{{ onClick }}</button>
 </template>
+
+<script setup>
+    import { ref } from 'vue'
+
+    const onClick = ref(() => {
+        console.log('click')
+    })
+</script>
